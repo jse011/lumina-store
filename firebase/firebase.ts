9bc550e40e74fc8a7623b6935fb13d145b089e5a
jsefao <55448613+jse011@users.noreply.github.com>
2026-04-28 13:01:54 -0500

    github

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDH2uW_kse7FtPfskJ71QprSO3KC4QogBQ',
-    authDomain: 'lumina-store-8409e197.firebaseapp.com',
-    databaseURL: 'https://lumina-store-8409e197-default-rtdb.firebaseio.com/',
-    projectId: 'lumina-store-8409e197',
-    storageBucket: 'lumina-store-8409e197.firebasestorage.app',
-    messagingSenderId: '613880054470',
-    appId: '1:613880054470:web:24622e177dc4f5331f8dc7',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inicializar Firebase
