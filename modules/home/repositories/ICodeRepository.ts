aa2d2f6eba2dc2a70e17d4bd7bdfd414f4481754
jsefao <55448613+jse011@users.noreply.github.com>
2026-05-01 13:52:30 -0500

    ...

--- modules/home/repositories/ICodeRepository.ts
+++ modules/home/repositories/ICodeRepository.ts
@@ -1,5 +1,5 @@
 export interface ICodeRepository {
   validateCode(code: string): Promise<boolean>;
-  assignCodeToUser(code: string, email: string): Promise<void>;
+  assignCodeToUser(code: string, userId: string, email: string): Promise<void>;
   isCodeAssigned(code: string): Promise<boolean>;
 }
